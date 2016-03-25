f416e8f7468769edebb09f361208e0d6f314c19d
Adauto Braz da Silva Neto <adautobraz.neto@gmail.com>
2016-03-25 15:27:16 -0300

    NoteContentProvider terminado

--- BlocodeNotas/app/src/main/java/com/example/android/blocodenotas/data/NoteContentProvider.java
+++ BlocodeNotas/app/src/main/java/com/example/android/blocodenotas/data/NoteContentProvider.java
@@ -1,13 +1,13 @@
 package com.example.android.blocodenotas.data;
 
 import android.content.ContentProvider;
-
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 
 import com.example.android.blocodenotas.utility.Constants;
 
@@ -19,11 +19,13 @@ import java.util.HashSet;
  */
 public class NoteContentProvider extends ContentProvider {
     private DatabaseHelper dbHelper;
+
     private static final String BASE_PATH_NOTE = "notes";
-    private static final in NOTE = 100;
+    private static final int NOTE = 100;
     private static final int NOTES = 101;
-    private static final String AUTHORITY = "com.example.blocodenotas.data.provider"
-    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY +"/" + BASE_PATH_NOTE)
+    private static final String AUTHORITY = "com.example.blocodenotas.data.provider";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY +"/" + BASE_PATH_NOTE);
+
     private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
         static {
             URI_MATCHER.addURI(AUTHORITY, BASE_PATH_NOTE,NOTES);
@@ -48,10 +50,10 @@ public class NoteContentProvider extends ContentProvider {
             case NOTES:
                 queryBuilder.appendWhere(Constants.COLUMN_ID + "=" + uri.getLastPathSegment());
             default:
-                throw new IllegalArgumentException("Unknown URI: " + uri)
+                throw new IllegalArgumentException("Unknown URI: " + uri);
         }
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor cursor = queryBuilder.query(db,projection,selection,selectionArgs,null, null, sortOrder)/
+        Cursor cursor =  queryBuilder.query(db,projection,selection,selectionArgs,null, null, sortOrder);
         cursor.setNotificationUri(getContext().getContentResolver(),uri);
         return cursor;
     }
@@ -63,17 +65,70 @@ public class NoteContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values){
-        return null;
+        int type = URI_MATCHER.match(uri);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Long id;
+        switch (type){
+            case NOTES:
+                id = db.insert(Constants.NOTES_TABLE, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return Uri.parse(BASE_PATH_NOTE + "/" + id);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs){
-        return 0;
+        int type = URI_MATCHER.match(uri);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int affectedRows;
+        switch (type) {
+            case NOTES:
+                affectedRows = db.delete(Constants.NOTES_TABLE, selection, selectionArgs);
+                break;
+
+            case NOTE:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    affectedRows = db.delete(Constants.NOTES_TABLE, Constants.COLUMN_ID + "=" + id, null);
+                } else {
+                    affectedRows = db.delete(Constants.NOTES_TABLE, Constants.COLUMN_ID + "=" + id + " and " + selection, selectionArgs);
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return affectedRows;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        int type = URI_MATCHER.match(uri);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int affectedRows;
+        switch (type) {
+            case NOTES:
+                affectedRows = db.update(Constants.NOTES_TABLE, values, selection, selectionArgs);
+                break;
+
+            case NOTE:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    affectedRows = db.update(Constants.NOTES_TABLE, values, Constants.COLUMN_ID + "=" + id, null);
+                } else {
+                    affectedRows = db.update(Constants.NOTES_TABLE, values, Constants.COLUMN_ID + "=" + id + " and " + selection, selectionArgs);
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return affectedRows;
     }
 
     private void checkColumns(String[] projection) {
