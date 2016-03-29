20007b44b1458633541b3937afe8a9a7263d32ef
Adauto Braz da Silva Neto <adautobraz.neto@gmail.com>
2016-03-29 15:32:02 -0300

    Falta funcionar a adição de notas

--- BlocodeNotas/app/src/main/java/com/example/android/blocodenotas/utility/Constants.java
+++ BlocodeNotas/app/src/main/java/com/example/android/blocodenotas/utility/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static final String NOTES_TABLE = "notes";
 
     public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
     public static final String COLUMN_TITLE = "title";
     public static final String COLUMN_CONTENT = "content";
     public static final String COLUMN_TAGS = "tags";
